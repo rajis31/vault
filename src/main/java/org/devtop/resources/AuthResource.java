c05af460ff1ad1b9c265449402c1d045845cdae8
rajis31 <rajsolan91@gmail.com>
2024-11-20 00:13:34 -0800

    Saving

--- src/main/java/org/devtop/resources/AuthResource.java
+++ src/main/java/org/devtop/resources/AuthResource.java
@@ -1,5 +1,6 @@
 package org.devtop.resources;
 
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -11,22 +12,38 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import java.util.HashMap;
+import java.util.Map;
 
 
 @Path("/auth")
 public class AuthResource {
+
     @Inject
     JsonWebToken jwt;
 
+
     @GET
-    @Path("/role")
-    @RolesAllowed({ "User", "Admin" })
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response helloRolesAllowed(@Context SecurityContext ctx){
-        HashMap<String, String> properties = new HashMap<>();
-        properties.put("role", String.valueOf(ctx.getUserPrincipal()));
-        return Response.ok().build(ctx.getUserPrincipal());
+    @Path("/jwt")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getJwt(){
+        return Response.ok(jwt).build();
     }
 
-
+    @GET
+    @Path("/role")
+    @PermitAll
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response helloRolesAllowed(){
+        try{
+            HashMap<String, String> properties = new HashMap<>();
+            properties.put("role", jwt.getClaim("role"));
+            properties.put("id", jwt.getClaim("id"));
+            return Response.ok(properties).build();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(Map.of("error", e.getMessage()))
+                    .build();
+        }
+    }
 }
